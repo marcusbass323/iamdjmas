ae84a4dd4f64d1c148a8e652abde28f6e6230b01
Marcus Bass <mbass@Marcuss-MBP.home>
2022-02-07 22:04:33 -0800

    init w routes created

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/social', function () {
+    return view('social');
+});
+
+Route::get('/videos', function () {
+    return view('videos');
+});
+
+Route::get('/events', function () {
+    return view('events');
+});
